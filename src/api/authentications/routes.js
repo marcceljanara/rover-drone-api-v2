68dc4319d50d87a814180f2930e05837103d39ba
Marccel Janara <nengahyuichiro34@gmail.com>
2025-03-07 21:50:16 +0700

    add versioning 1 autentications endpoint

--- src/api/authentications/routes.js
+++ src/api/authentications/routes.js
@@ -3,9 +3,9 @@ import express from 'express';
 const authenticationRoutes = (handler) => {
   const router = express.Router();
 
-  router.post('/authentications', handler.postAuthenticationHandler);
-  router.put('/authentications', handler.putAuthenticationHandler);
-  router.delete('/authentications', handler.deleteAuthenticationHandler);
+  router.post('/v1/authentications', handler.postAuthenticationHandler);
+  router.put('/v1/authentications', handler.putAuthenticationHandler);
+  router.delete('/v1/authentications', handler.deleteAuthenticationHandler);
 
   return router;
 };
