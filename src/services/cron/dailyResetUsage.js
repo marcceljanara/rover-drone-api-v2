d59197d9527b2f202d2e6416f0380db8997926a3
Marccel Janara <nengahyuichiro34@gmail.com>
2025-06-05 00:03:16 +0700

    create crontab daily reset usage

---
+++ src/services/cron/dailyResetUsage.js
@@ -0,0 +1,23 @@
+import cron from 'node-cron';
+import pkg from 'pg';
+
+const { Pool } = pkg;
+const pool = new Pool();
+
+cron.schedule('0 0 * * *', async () => {
+  console.log('[Cron] Reset log pemakaian harian perangkat...');
+
+  try {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    await pool.query(`
+      DELETE FROM device_usage_logs
+      WHERE start_time < $1
+    `, [today]);
+
+    console.log('[Cron] Reset log harian berhasil.');
+  } catch (err) {
+    console.error('[Cron] Gagal mereset log:', err);
+  }
+});
