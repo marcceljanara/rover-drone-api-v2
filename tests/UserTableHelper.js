ee574f329575e16a5c9e5eff4948262be9fee27f
Marccel Janara <nengahyuichiro34@gmail.com>
2025-06-06 00:37:16 +0700

    create function helper for testing addresses

--- tests/UserTableHelper.js
+++ tests/UserTableHelper.js
@@ -50,8 +50,46 @@ const UsersTableTestHelper = {
     return result.rows[0].otp_code;
   },
 
+  async addAddress(userId, {
+    id = 'address-123',
+    namaPenerima = 'I Nengah Marccel',
+    noHp = '085212345678',
+    alamatLengkap = 'Jalan Bali Indah, depan beringin RT X/RW X',
+    provinsi = 'Lampung',
+    kabupatenKota = 'Lampung Timur',
+    kecamatan = 'Raman Utara',
+    kelurahan = 'Rejo Binangun',
+    kodePos = '34371',
+    isDefault = false,
+  }) {
+    const query = {
+      text: `INSERT INTO user_addresses (
+        id, user_id, nama_penerima, no_hp, alamat_lengkap, 
+        provinsi, kabupaten_kota, kecamatan, kelurahan, kode_pos, is_default
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+      RETURNING id`,
+      values: [
+        id, userId, namaPenerima, noHp, alamatLengkap,
+        provinsi, kabupatenKota, kecamatan, kelurahan, kodePos, isDefault,
+      ],
+    };
+    const result = await pool.query(query);
+    return result.rows[0].id;
+  },
+
+  async findAddressById(id) {
+    const query = {
+      text: 'SELECT * FROM user_addresses WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await pool.query(query);
+    return result.rows[0];
+  },
+
   async cleanTable() {
     await pool.query('DELETE FROM users WHERE 1=1');
+    await pool.query('DELETE FROM user_addresses WHERE 1=1');
   },
 };
 
