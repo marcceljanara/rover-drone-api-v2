2b9f43ca8241a98d7dff533316f209419c227f11
Marccel Janara <nengahyuichiro34@gmail.com>
2025-06-09 21:57:59 +0700

    fix integration test ReportsService

--- src/services/postgres/_test/ReportsService.test.js
+++ src/services/postgres/_test/ReportsService.test.js
@@ -47,8 +47,14 @@ describe('ReportsService', () => {
     await DevicesTableTestHelper.addDevice({ id: 'device-456' });
     const addressId = await UsersTableTestHelper.addAddress(user1, { id: 'address-123' });
     const addressId2 = await UsersTableTestHelper.addAddress(user2, { id: 'address-456' });
-    const paymentId1 = (await rentalsService.addRental(user1, 6, 'user', addressId, 500000)).payment_id;
-    const paymentId2 = (await rentalsService.addRental(user2, 6, 'user', addressId2, 500000)).payment_id;
+    const payloadRental = {
+      shippingName: 'JNE',
+      serviceName: 'JTR23',
+      shippingCost: 500000,
+      etd: '4',
+    };
+    const paymentId1 = (await rentalsService.addRental(user1, 6, 'user', addressId, payloadRental)).payment_id;
+    const paymentId2 = (await rentalsService.addRental(user2, 6, 'user', addressId2, payloadRental)).payment_id;
     const payload1 = {
       id: paymentId1,
       paymentStatus: 'completed',
