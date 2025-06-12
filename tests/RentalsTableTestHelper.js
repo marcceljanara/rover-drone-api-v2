496212114d8f93e6699ee642f9ff2f916da89a98
Marccel Janara <nengahyuichiro34@gmail.com>
2025-06-12 12:51:03 +0700

    add helper test rental extension

--- tests/RentalsTableTestHelper.js
+++ tests/RentalsTableTestHelper.js
@@ -16,7 +16,25 @@ const RentalsTableTestHelper = {
     await pool.query('DELETE FROM rental_sensors WHERE 1=1');
     await pool.query('DELETE FROM shipment_orders WHERE 1=1');
     await pool.query('DELETE FROM rental_shipping_infos WHERE 1=1');
+    await pool.query('DELETE FROM rental_extensions WHERE 1=1');
+  },
+
+  async findRentalExtensionById(id) {
+    const query = {
+      text: 'SELECT * FROM rental_extensions WHERE id = $1',
+      values: [id],
+    };
+    const result = await pool.query(query);
+    return result.rows[0];
+  },
+
+  async findRentalCostById(id) {
+    const query = {
+      text: 'SELECT * FROM rental_costs WHERE rental_id = $1',
+      values: [id],
+    };
+    const result = await pool.query(query);
+    return result.rows[0];
   },
 };
-
 export default RentalsTableTestHelper;
