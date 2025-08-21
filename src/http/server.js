39c608d154b809696cc7732cb6bf858b4ece94d8
Marccel Janara <nengahyuichiro34@gmail.com>
2025-08-21 08:52:45 +0700

    fix: reorder CORS configuration for improved clarity and consistency

--- src/http/server.js
+++ src/http/server.js
@@ -53,12 +53,12 @@ function createServer() {
   const app = express();
   app.use(cors({
     origin: ['*'],
-    // methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    // allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // ⬅️ aktifkan kalau pakai cookie / token JWT dengan Auth header
   }));
 
-  // app.options('*', cors()); // ⬅️ Wajib biar preflight dijawab
+  app.options('*', cors()); // ⬅️ Wajib biar preflight dijawab
   app.use(express.json());
   app.use(helmet.hidePoweredBy());
   app.use(helmet.noSniff()); // Mencegah MIME-sniffing
