1e80d8e11a4c763c699bcc96b7c49e6f471be189
Marccel Janara <nengahyuichiro34@gmail.com>
2025-09-05 21:11:34 +0700

    add validation change password

--- src/validator/users/index.js
+++ src/validator/users/index.js
@@ -17,8 +17,21 @@ const UsersValidator = {
     if (result.error) throw new InvariantError(result.error.message);
   },
 
+  validateEmailPayload: (payload) => {
+    const result = UserSchema.emailPayload.validate(payload);
+    if (result.error) throw new InvariantError(result.error.message);
+  },
+
   validateAddressPayload: (payload) => {
     const result = UserSchema.addressPayload.validate(payload);
+    if (result.error) throw new InvariantError(result.error.message);
+  },
+  validateTokenPayload: (payload) => {
+    const result = UserSchema.tokenPayload.validate(payload);
+    if (result.error) throw new InvariantError(result.error.message);
+  },
+  validateChangePasswordPayload: (payload) => {
+    const result = UserSchema.changePasswordPayload.validate(payload);
     if (result.error) throw new InvariantError(result.error.message);
   },
 };
