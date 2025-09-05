c208eb18edd11c7dc0787f5aa1343e70355027d3
Marccel Janara <nengahyuichiro34@gmail.com>
2025-09-05 15:53:02 +0700

    update migration, revert username not null = true

--- migrations/1755831723951_alter-table-users-drop-column-password,-otp,-otp-expiry.cjs
+++ migrations/1755831723951_alter-table-users-drop-column-password,-otp,-otp-expiry.cjs
@@ -6,9 +6,6 @@ exports.up = (pgm) => {
   pgm.dropColumn('users', 'password');
   pgm.dropColumn('users', 'otp_code');
   pgm.dropColumn('users', 'otp_expiry');
-
-  // ubah username dari NOT NULL -> NULLABLE
-  pgm.alterColumn('users', 'username', { notNull: false });
 };
 
 exports.down = (pgm) => {
@@ -18,7 +15,4 @@ exports.down = (pgm) => {
     otp_code: { type: 'VARCHAR(6)' },
     otp_expiry: { type: 'TIMESTAMP' },
   });
-
-  // ubah kembali username jadi NOT NULL
-  pgm.alterColumn('users', 'username', { notNull: true });
 };
