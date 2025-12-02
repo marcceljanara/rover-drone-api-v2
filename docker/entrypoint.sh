fce8c0cd1abe5fc50b563714582a4395831adcff
Marccel Janara <nengahyuichiro34@gmail.com>
2025-12-02 14:36:20 +0700

    fix: update entrypoint script to use environment variables for database connection

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,19 +2,17 @@
 set -e
 
 echo "⏳ Waiting for Postgres..."
-until nc -z db 5432; do
+until nc -z "$PGHOST" "$PGPORT"; do
+  echo "Waiting for $PGHOST:$PGPORT..."
   sleep 1
 done
 echo "✅ Postgres is up!"
 
-# Jalankan migrasi
 echo "🚀 Running migrations..."
 npm run migrate up || true
 
-# (Opsional) Generate admin default
 echo "👤 Generating default admin (skip if exists)..."
 npm run generate-admin || true
 
-# Start server
 echo "🔗 Starting server..."
 npm run start
